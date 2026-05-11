57e3a6b5b8c9dd156919f124a2adda7b718da841
ozingeodari <125386542+ozingeodari@users.noreply.github.com>
2026-05-11 10:35:18 +0900

    [level 1] Title: 최소직사각형, Time: 0.80 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        
+        for (int i=0; i<sizes.length; i++) {
+            if (sizes[i][1] > sizes[i][0]) {
+                int temp = sizes[i][1];
+                sizes[i][1] = sizes[i][0];
+                sizes[i][0] = temp;
+            }
+        }
+        
+        int maxW = 0;
+        int maxH = 0;
+        for (int i=0; i<sizes.length; i++) {
+            if (sizes[i][0] > maxW) {
+                maxW = sizes[i][0];
+            }
+            if (sizes[i][1] > maxH) {
+                maxH = sizes[i][1];
+            }
+        }
+        
+        answer = maxW * maxH;
+        
+        return answer;
+    }
+}
